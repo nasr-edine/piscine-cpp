711d623e8945f3e04c70561ad296f0f639a3438c
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2019-03-26 21:42:54 +0100

    add ressources

---
+++ ex02/Account.class.cpp
@@ -0,0 +1,26 @@
+#include "Account.class.hpp"
+
+static int	t::getNbAccounts( void )
+{
+    return _nbAccounts;
+}
+
+static int	getTotalAmount( void );
+{
+    return _totalAmount;
+}
+
+static int	getNbDeposits( void )
+{
+    return _totalNbDeposits;
+}
+
+static int	getNbWithdrawals( void )
+{
+    return _nbWithdrawals();
+}
+
+static void	displayAccountsInfos( void )
+{
+    
+}
